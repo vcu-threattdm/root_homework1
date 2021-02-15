51f649324f64044cde901a7bf67f1c222c0650ad
vcu-threattdm <78376316+vcu-threattdm@users.noreply.github.com>
2021-02-15 15:04:20 -0500

    Update hw1.py

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -4,35 +4,35 @@
 def return_number_3():
     """ This function should return an integer with the value of 3"""
 
-    return_value = None
-    return return_value
+    return_value = "3"
+    return return_number_3
 
 
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
 
-    return_value = None
-    return return_value
+    return_value = "vcu"
+    return return_string_vcu
 
 
 def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
     Return it but the lowercase version of it."""
 
-    return_value = input_string
-    return return_value
+    return_value = lower
+    return return_lowercased_string
 
 
 def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
-    return_value = input_string
-    return return_value
+    return_value = strip
+    return return_without_starting_ending_whitespace
 
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
 
-    return_value = None
-    return return_value
+    return_value = 2 + 2
+    return return_addition
