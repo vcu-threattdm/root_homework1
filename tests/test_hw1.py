542bd2033a0e250bdedb35607e55e1478abd8ad7
vcu-threattdm <78376316+vcu-threattdm@users.noreply.github.com>
2021-02-15 15:11:30 -0500

    Update test_hw1.py

--- tests/test_hw1.py
+++ tests/test_hw1.py
@@ -7,22 +7,22 @@ from homework1 import hw1
 def test_return_number_3():
     """Make sure the return of the function is 3, integer"""
 
-    return_number_3() = 3
-    
+    assert hw1.return_number_3() == 3
+
 
 def test_return_string_vcu():
     """Make sure the return of the function is the string vcu"""
 
-   return return_string_vcu() = "vcu"
+    assert hw1.return_string_vcu() == "vcu"
 
 
 def test_return_lowercased_string():
     """Make sure the return of the function is the lowercased version of the parameter."""
 
-    return_lowercased_string("HI THERE MOM") = "hi there mom"
-    return_lowercased_string("vcu.edu") = "vcu.edu"
-    (
-        return_lowercased_string("We Wish You a Merry Monday")
+    assert hw1.return_lowercased_string("HI THERE MOM") == "hi there mom"
+    assert hw1.return_lowercased_string("vcu.edu") == "vcu.edu"
+    assert (
+        hw1.return_lowercased_string("We Wish You a Merry Monday")
         == "we wish you a merry monday"
     )
 
@@ -30,16 +30,16 @@ def test_return_lowercased_string():
 def test_return_without_starting_ending_whitespace():
     """Make sure the function strips the whitespace """
 
-    (
-        return_without_starting_ending_whitespace("   asdfasdf    ") = "asdfasdf"
+    assert (
+        hw1.return_without_starting_ending_whitespace("   asdfasdf    ") == "asdfasdf"
     )
 
 
 def test_return_addition():
     """ Make sure the function returns the sum of the two parameters. """
 
-    return_addition(1, 2) = 3
-    return_addition(100, 200) = 300
-    return_addition(43, 43) = 86
-    return_addition(50, 25) = 75
-    return_addition(19, -19) = 0
+    assert hw1.return_addition(1, 2) == 3
+    assert hw1.return_addition(100, 200) == 300
+    assert hw1.return_addition(43, 43) == 86
+    assert hw1.return_addition(50, 25) == 75
+    assert hw1.return_addition(19, -19) == 0
